c397de3e491823e5a3f15c612cd346928f1d5d03
TrentWells5 <jtrentw143@gmail.com>
2024-02-06 14:30:36 -0600

    im restarting cause im annoyed

--- project1/parser.hpp
+++ project1/parser.hpp
@@ -1,37 +1,14 @@
 #ifndef PARSER_HPP
 #define PARSER_HPP
 
-#include "scanner.hpp" // Adjust the include path according to your project structure
-#include <string>
-#include <iostream>
-#include <algorithm>
-#include <cassert>
+#include "scanner.hpp"
 using namespace std;
 
 class Parser {
 public:
-    Parser(const string& source);
-    void parse();
 
 private:
-    Scanner scanner;
-    string currentToken;
-    string currentValue;
-
-    void error(const string& message);
-    void scan();
-    void expect(const initializer_list<string>& symbolSet);
-
-    // Grammar rules
-    void Stmts();
-    void Stmt();
-    void Assign();
-    void Cond();
-    void Loop();
-    void LVal();
-    void AExpr();
-    void Term();
-    void Factor();
+   
 };
 
 #endif
--- project1/scanner.hpp
+++ project1/scanner.hpp
@@ -1,58 +1,13 @@
 #ifndef SCANNER_HPP
 #define SCANNER_HPP
 
-#include <string>
-#include <unordered_set>
-#include <cctype>
-#include <iostream>
-#include <unordered_map>
 using namespace std;
 
 class Scanner {
 public:
-    Scanner(const string& source);
-    void init();
-    pair<string, string> nextToken();
-    static const string eoIToken;
 
 private:
-    string source;
-    size_t position;
-    string currentText;
-    string currentToken;
-
-    // Character classes
-    static const char START_COMMENT;
-    static const char EQUAL;
-
-    static const unordered_set<char> WHITESPACE;
-    static const unordered_set<char> DIGITS;
-    static const unordered_set<char> LETTERS;
-    static const unordered_set<char> LETTERS_OR_DIGITS;
-
-    static const unordered_map<char, string> OP_TABLE;
-    static const unordered_map<string, string> KEYWORD_TABLE;
-
-    // Helper methods
-    void error(const string& message);
-    char currentCh();
-    void move();
-    bool atEOI();
-    void eat();
-    string find(char x);
-    string findStar(const unordered_set<char>& s);
-    string skip(char x);
-    string skipStar(const unordered_set<char>& s);
-    void skipWS();
-    void skipComment();
-    void jump();
-    void jumpStar();
-    pair<string, string> NUM();
-    pair<string, string> ID();
-    pair<string, string> STRI();
-    pair<string, string> twoCharSym(char secondCh, const string& firstToken, const string& secondToken);
-    static unordered_set<char> initializeLetters();
-    static unordered_set<char> initializeLettersOrDigits();
+    
 };
 
 #endif
